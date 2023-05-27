730afc5c663e4134ef9188c17923f32fd7e06cce
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-05-27 23:39:23 +0530

    commit3

--- script.js
+++ script.js
@@ -2,20 +2,24 @@
 function appendOnWebPage(data) {
     console.log(data.length);
     let container = document.getElementById("list");
+    container.innerHTML = "";
 
     for(let i=0; i<data.length; i++) {
         let obj = data[i];
 
-        // console.log(obj);
         let listItem = document.createElement("tr");
         console.log(listItem);
-        //        
+        // inner content
         let td1 = `<td><img src="${obj.image}"> ${obj.name}</td>`;
         let td2 = `<td>${obj.symbol}</td>`;
         let td3 = `<td>$${obj.current_price}</td>`;
         let td4 = `<td>$${obj.market_cap}</td>`;
-        let td5 = `<td>${obj.price_change_percentage_24h}%</td>`;
+        let td5 = `<td class="green">${obj.price_change_percentage_24h}%</td>`;
+        if(obj.price_change_percentage_24h < 0) {
+            td5 = `<td class="red">${obj.price_change_percentage_24h}%</td>`;
+        }
         let td6 = `<td>Mkt Cap : $${obj.market_cap}</td>`;
+
         listItem.innerHTML = td1 + td2 + td3 + td4 + td5 + td6;
         //Adding to web-page
         container.append(listItem);
@@ -27,14 +31,14 @@ function appendOnWebPage(data) {
 let response = fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
 
 
-// console.log(response);
 response.then((data) => {
     let prom = data.json();
 
     prom.then((arr) => {
-        console.log(arr);
+        // console.log(arr);
         appendOnWebPage(arr);
-        console.log("after");
+        allData = arr;//making it available in global context
+        // console.log("after");
     });
 
     prom.catch((message) => {
@@ -42,7 +46,10 @@ response.then((data) => {
     });
 });
 
-// response.catch((message) => {
-//     console.log("catch method");
-//     console.log(message);
-// });
+let allData;
+function sortByMktCap() {
+    appendOnWebPage(allData.sort((a, b) => {return a.market_cap - b.market_cap}));
+}
+function sortByPercentage() {
+    appendOnWebPage(allData.sort((a, b) => {return a.price_change_percentage_24h - b.price_change_percentage_24h}));
+}
